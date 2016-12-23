61b842280c35ef7e91965b8ac87fb83c0bf8d154
1047239335@qq.com <912513404whh>
2016-12-23 11:21:32 +0800

    activity

--- app/src/main/java/weather/wu/com/weather/MainActivity.java
+++ app/src/main/java/weather/wu/com/weather/MainActivity.java
@@ -1,9 +1,10 @@
 package weather.wu.com.weather;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
