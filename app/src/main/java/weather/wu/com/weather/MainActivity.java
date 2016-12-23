e78dbe27c911f62500c0e7047f83368e1e85b8e8
1047239335@qq.com <912513404whh>
2016-12-23 11:16:46 +0800

    Initial commit

---
+++ app/src/main/java/weather/wu/com/weather/MainActivity.java
@@ -0,0 +1,13 @@
+package weather.wu.com.weather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
