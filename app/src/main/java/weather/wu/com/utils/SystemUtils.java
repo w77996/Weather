1a379aa0ad53b9ac83844f74ed765177be569047
1047239335@qq.com <912513404whh>
2016-12-26 17:00:10 +0800

    add okhttp and HttpUtil.java

--- app/src/main/java/weather/wu/com/utils/SystemUtils.java
+++ app/src/main/java/weather/wu/com/utils/SystemUtils.java
@@ -2,7 +2,12 @@ package weather.wu.com.utils;
 
 import android.content.Context;
 import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.WindowManager;
+
+import java.lang.reflect.Field;
+
+import weather.wu.com.weather.R;
 
 /**
  * Created by 吴海辉 on 2016/12/24.
@@ -10,7 +15,6 @@ import android.view.WindowManager;
 public class SystemUtils {
     /**
      * 获取手机屏幕高度
-     *
      * @param context
      * @return
      */
@@ -22,10 +26,8 @@ public class SystemUtils {
         wm.getDefaultDisplay().getMetrics(dm);
         return dm.heightPixels;
     }
-
     /**
      * 获取手机屏幕宽度
-     *
      * @param context
      * @return
      */
@@ -36,5 +38,39 @@ public class SystemUtils {
         // 获取屏幕信息
         wm.getDefaultDisplay().getMetrics(dm);
         return dm.widthPixels;
+    }
+
+    /**
+     * 反射方法获取状态栏高度
+     * @param context
+     * @return
+     */
+    public static int getStatusBarHeight(Context context) {
+        int statusBarHeight = 20;
+        try {
+            Class<?> _class = Class.forName("com.android.internal.R$dimen");
+            Object object = _class.newInstance();
+            Field field = _class.getField("status_bar_height");
+            int restult = Integer.parseInt(field.get(object).toString());
+            statusBarHeight = context.getResources().getDimensionPixelSize(
+                    restult);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // Toast.makeText(getActivity(), "StatusBarHeight = " + statusBarHeight,
+        // Toast.LENGTH_SHORT).show();
+        return statusBarHeight;
+    }
+
+    /**
+     * 获取?attr/actionBarSize高度
+     * @param context
+     * @return
+     */
+    public static int getActionBarSize(Context context){
+        TypedValue typedValue = new TypedValue();
+       context.getTheme().resolveAttribute(R.attr.actionBarSize, typedValue, true);
+        int actionBarHeight = TypedValue.complexToDimensionPixelSize(typedValue.data, context.getResources().getDisplayMetrics());
+        return actionBarHeight;
     }
 }
--- app/src/main/java/weather/wu/com/weather/MainActivity.java
+++ app/src/main/java/weather/wu/com/weather/MainActivity.java
@@ -2,40 +2,53 @@ package weather.wu.com.weather;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
+import butterknife.ButterKnife;
+import weather.wu.com.utils.SystemUtils;
+
 
 public class MainActivity extends AppCompatActivity {
-    private int mHeaderHeight = -1;
-    RelativeLayout mRelativeLayout;
+    private int mNowWeatherHeight = -1;
+    private RelativeLayout mNowWeatherRelativeLayout;
+    private Context mContext = MainActivity.this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         initView();
     }
 
     private void initView() {
        //  = SystemUtils.getDisplayHeight(getActivity());
-        mRelativeLayout = (RelativeLayout)findViewById(R.id.rl);
-        WindowManager wm = (WindowManager) MainActivity.this
-                .getSystemService(Context.WINDOW_SERVICE);
-        DisplayMetrics dm = new DisplayMetrics();
-        wm.getDefaultDisplay().getMetrics(dm);
-        int displayHeight = dm.heightPixels;
+        mNowWeatherRelativeLayout = (RelativeLayout)findViewById(R.id.main_now_weather);
+        //获取屏幕高度
+       /* int displayHeight = SystemUtils.getDisplayHeight(MainActivity.this);
         Log.e("Log displayHeight",displayHeight+"");
-        // HeaderView高度=屏幕高度-标题栏高度-状态栏高度
-        mHeaderHeight = displayHeight
-                - getResources().getDimensionPixelSize(
-                R.dimen.abs__action_bar_default_height)
-                ;
-        mRelativeLayout.setLayoutParams(new LinearLayout.LayoutParams(
-               LinearLayout.LayoutParams.MATCH_PARENT, mHeaderHeight));
+
+        TypedValue typedValue = new TypedValue();
+         MainActivity.this.getTheme().resolveAttribute(R.attr.actionBarSize, typedValue, true);
+         int[] attribute = new int[] { android.R.attr.textSize };
+       TypedArray array =   MainActivity.this.obtainStyledAttributes(typedValue.resourceId, attribute);
+        Log.e("Log array",array+"");
+        int textSize = array.getDimensionPixelSize(0 *//* index *//*, -1 *//* default size *//*);
+        array.recycle();
+        Log.e("Log typedValue",textSize+"");
+        int actionBarHeight = TypedValue.complexToDimensionPixelSize(typedValue.data, MainActivity.this.getResources().getDisplayMetrics());
+        Log.e("Log actionBarHeight",actionBarHeight+"");
+       Log.e("Log System.getActionBarHeight",);*/
+        // mNowWeatherHeight高度=屏幕高度-标题栏高度-状态栏高度
+        mNowWeatherHeight = SystemUtils.getDisplayHeight(mContext)- SystemUtils.getActionBarSize(mContext)- SystemUtils.getStatusBarHeight(mContext);;
+        //设置当前天气信息RelativeLayout的高度
+        mNowWeatherRelativeLayout.setLayoutParams(new LinearLayout.LayoutParams( LinearLayout.LayoutParams.MATCH_PARENT, mNowWeatherHeight));
     }
 }
